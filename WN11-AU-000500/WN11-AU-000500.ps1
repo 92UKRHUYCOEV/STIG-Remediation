a7aae08013b3c51177cc9d246f720e935361fce4
Sallie Chait <254045157+92UKRHUYCOEV@users.noreply.github.com>
2026-08-22 12:15:51 -0400

    Update WN11-AU-000500.ps1

--- WN11-AU-000500/WN11-AU-000500.ps1
+++ WN11-AU-000500/WN11-AU-000500.ps1
@@ -16,7 +16,7 @@
     Version         : 1.1
     STIG-ID         : WN11-AU-000500
     Severity        : Medium (CAT II)
-    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000500/
+    Documentation   : https://stigaview.com/products/win11/v2r8/WN11-AU-000500/
 #>
 
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
