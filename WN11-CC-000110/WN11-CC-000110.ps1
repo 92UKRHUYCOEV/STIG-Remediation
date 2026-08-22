9f3e29ee171df8ff76cef8520440d6e564c9025f
Sallie Chait <254045157+92UKRHUYCOEV@users.noreply.github.com>
2026-08-22 12:19:18 -0400

    Update WN11-CC-000110.ps1

--- WN11-CC-000110/WN11-CC-000110.ps1
+++ WN11-CC-000110/WN11-CC-000110.ps1
@@ -15,11 +15,9 @@
     Date Created    : 04-22-26
     Last Modified   : 08-22-26
     Version         : 1.1
-    CVEs            : N/A
-    Plugin IDs      : N/A
     STIG-ID         : WN11-CC-000110
     Severity        : Medium (CAT II)
-   Reference        : https://stigaview.com/products/win11/v2r7/WN11-CC-000110/
+    Reference       : https://stigaview.com/products/win11/v2r8/WN11-CC-000110/
 
 #>
 
